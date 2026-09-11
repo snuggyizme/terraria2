ccf83350dc70811f31cf083de147fbcdedb175fa
snuggyizme <snuggyizme@gmail.com>
2026-09-11 16:39:13 +1000

    WIP Structures

---
+++ scripts/resourceDefinitions/generationPasses/structureGenerationPass.gd
@@ -0,0 +1,5 @@
+class_name StructureGenerationPass extends GenerationPass
+
+func generate(context: GenerationContext) -> void:
+	var structures: Dictionary[Vector2i, Structure] = context.world.
+	
--- scripts/resourceDefinitions/worldGenerationContext.gd
+++ scripts/resourceDefinitions/worldGenerationContext.gd
@@ -2,9 +2,13 @@ class_name WorldGenerationContext extends RefCounted
 
 var mySeed: int = 0
 var dimension: Vector2i
+var structures: Dictionary[Vector2i, Structure] = {}
 
 func _init(
 	sizeInChunks: Vector2i
 ) -> void:
 	dimension = sizeInChunks
 	mySeed = randi()
+
+func generateStructures() -> void:
+	pass
